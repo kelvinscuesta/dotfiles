e33ef336ea84190d6b12a07a4df1de59547c9612
Kelvin Cuesta <kelvin.cuesta@gusto.com>
2026-01-12 17:16:56 -0500

    new comments for lazy.lua

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,7 +1,8 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+-- Bootstrap: auto-install lazy.nvim if not present
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim' -- ~/.local/share/nvim/lazy/lazy.nvim
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  -- clone with blob:none for faster download (no file history)
   local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
@@ -13,43 +14,41 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     os.exit(1)
   end
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath) -- add lazy.nvim to runtime path
 
--- Setup lazy.nvim
+-- Plugin Manager Setup
 require('lazy').setup {
+  -- Plugin Sources
   spec = {
-    -- import your plugins
-    { import = 'plugins' },
+    { import = 'plugins' }, -- load all files from lua/plugins/
   },
+
+  -- Default Behavior
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
+    lazy = false, -- load plugins at startup (set true for lazy-load by default)
+    version = false, -- use latest commit, not releases (many plugins have outdated tags)
   },
-  -- automatically check for plugin updates
+
+  -- Update Checker: runs in background, no notifications
   checker = { enabled = true, notify = false },
+
+  -- Performance: disable unused built-in plugins
   performance = {
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
-        'gzip',
-        -- "matchit",
-        -- "matchparen",
-        'netrwPlugin',
-        'tarPlugin',
-        'tohtml',
-        'tutor',
-        'zipPlugin',
+        'gzip', -- editing gzipped files
+        'netrwPlugin', -- built-in file explorer (using other plugins)
+        'tarPlugin', -- editing tar archives
+        'tohtml', -- :TOhtml command
+        'tutor', -- :Tutor command
+        'zipPlugin', -- editing zip archives
       },
     },
   },
+
+  -- UI: fallback icons for non-nerd-font terminals
   {
     ui = {
-      -- If you are using a Nerd Font: set icons to an empty table which will use the
-      -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
       icons = vim.g.have_nerd_font and {} or {
         cmd = '⌘',
         config = '🛠',
