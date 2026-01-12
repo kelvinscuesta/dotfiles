08e8dade79fe71d76a6ca3474b64ae70bfa96bd5
Kelvin Cuesta <kelvin.cuesta@gusto.com>
2026-01-12 17:28:19 -0500

    add comments for autopairs

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,6 +1,8 @@
--- Autopairs: auto-close brackets, quotes, etc.
+-- Autopairs: automatically insert closing brackets, quotes, etc.
+-- Type ( and it inserts (), placing cursor between them
+-- Works with: () [] {} '' "" `` and language-specific pairs
 return {
   'windwp/nvim-autopairs',
-  event = 'InsertEnter',
+  event = 'InsertEnter', -- load when entering insert mode (lazy)
   opts = {},
 }
