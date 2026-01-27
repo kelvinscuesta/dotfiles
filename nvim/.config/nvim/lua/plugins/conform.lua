af433577f492ca7ffffc445b56570ace8ae4e1ff
Kelvin Cuesta <kelvin.cuesta@gusto.com>
2026-01-27 11:15:26 -0500

    change to zdiff3 and add oxfmt to conform nvim

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -39,14 +39,14 @@ return {
     formatters_by_ft = {
       lua = { 'stylua' },
       python = { 'ruff_organize_imports', 'ruff_format' }, -- sort imports, then format
-      javascript = { 'prettierd', 'prettier', stop_after_first = true },
-      typescript = { 'prettierd', 'prettier', stop_after_first = true },
-      javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-      css = { 'prettierd', 'prettier', stop_after_first = true },
-      html = { 'prettierd', 'prettier', stop_after_first = true },
-      json = { 'prettierd', 'prettier', stop_after_first = true },
-      graphql = { 'prettierd', 'prettier', stop_after_first = true },
+      javascript = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+      html = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+      json = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+      graphql = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
     },
   },
 }
